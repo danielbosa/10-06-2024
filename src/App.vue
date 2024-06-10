9dd93798283cf25bd716d4536a250ed1ecd574f4
danielbosa <danielbosa95@gmail.com>
2024-06-10 21:27:38 +0200

    API call setup

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,36 @@
 <template>
   <h1 class="text-center">Ciao</h1>
   <i class="fa fa-solid fa-home"></i>
+  <ul>
+    <li v-for="project in projects" :key="project.id">{{ project.title }}
+      <img :src="store.imagBasePath + project.image" :alt="project.title">
+    </li>
+  </ul>
 </template>
 
 <script>
+  import {store} from './store.js';
+  import axios from 'axios';
   export default {
-    name: 'App'
+    name: 'App',
+    data(){
+      return{
+        store,
+        projects : []
+      }
+    },
+    methods: {
+      getAllProjects(){
+        axios.get(this.store.apiBaseUrl + '/projects').then((res) =>{
+          console.log(res.data)
+          this.projects = res.data.results;
+
+        })
+      }
+    },
+    mounted(){
+      this.getAllProjects();
+    }
   }
 </script>
 
---
+++ src/store.js
@@ -0,0 +1,6 @@
+import {reactive} from 'vue';
+export const store = reactive({
+    apiBaseUrl: 'http://127.0.0.1:8000/api',
+    //images path bc store in storage on server
+    imagBasePath: 'http://127.0.0.1:8000/storage/',
+})
