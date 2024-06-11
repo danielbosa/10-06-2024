ebfe1b4c7e0155fe0b5eb75a2097112b4591b0bf
danielbosa <danielbosa95@gmail.com>
2024-06-11 23:44:31 +0200

    Added not-found route

--- src/router.js
+++ src/router.js
@@ -1,6 +1,8 @@
 import { createRouter, createWebHistory } from "vue-router";
 import AppHome from './pages/AppHome.vue';
 import ProjectList from './pages/ProjectList.vue';
+import ProjectComponent from './pages/ProjectComponent.vue';
+import NotFound from './pages/NotFound.vue';
 
 const router = createRouter({
     history: createWebHistory(),
@@ -14,6 +16,17 @@ const router = createRouter({
             path: "/projects",
             name: "projects",
             component: ProjectList,
+        },
+        {
+            path: "/projects/:slug",
+            name: "single-project",
+            component: ProjectComponent,
+        },
+        {
+            //fallback route --> qualunque cosa non abbia matchato con path di rotte precedenti, allora matcha qui e rimanda alla not found
+            path: "/:pathMatch(.*)*",
+            name: "not-found",
+            component: NotFound,
         },
     ]
 });
