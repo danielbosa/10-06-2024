bb683b9b8ede40fe96e84a36dae67a150648a458
danielbosa <danielbosa95@gmail.com>
2024-06-12 09:41:19 +0200

    array to store type from api

--- src/store.js
+++ src/store.js
@@ -3,4 +3,5 @@ export const store = reactive({
     apiBaseUrl: 'http://127.0.0.1:8000/api',
     //images path bc store in storage on server
     imagBasePath: 'http://127.0.0.1:8000/storage/',
+    types: [],
 })
