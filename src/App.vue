1b52356ce4e22774036382dc84ded021b0ef37b3
danielbosa <danielbosa95@gmail.com>
2024-06-12 09:40:52 +0200

    computed to get types from api

--- src/App.vue
+++ src/App.vue
@@ -8,14 +8,32 @@
 </template>
 
 <script>
-  import HeaderComponent from './components/HeaderComponent.vue'
+  import HeaderComponent from './components/HeaderComponent.vue';
+  import { store } from './store';
+  import axios from 'axios';
   export default {
     name: 'App',
     components: {
       HeaderComponent
     },
-    
+    data() {
+    return {
+      store
+    }
+  },
+  methods: {
+        getTypes() {
+        axios.get(this.store.apiBaseUrl + '/types').then((res) => {
+          console.log(res.data);
+          this.store.types = res.data.results;
+        });
+    }
+  },
+  mounted() {
+    this.getTypes();
   }
+}
+
 </script>
 
 <style lang="scss" scoped>
