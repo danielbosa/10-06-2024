47d8f67bec2bbd948c5107fd2d96c1f6118b165a
danielbosa <danielbosa95@gmail.com>
2024-06-12 09:43:58 +0200

    Inserted CardComponent

--- src/pages/ProjectList.vue
+++ src/pages/ProjectList.vue
@@ -1,19 +1,30 @@
 <template>
-    <ul>
-    <li v-for="project in projects" :key="project.id">{{ project.title }}
-        <img :src="store.imagBasePath + project.image" :alt="project.title">
-        <div>{{ project.type?.name }}</div>
+    <div class="d-flex justify-content-between align-items-center">
+        <h1>All Projects</h1>
+        <select name="type" id="type">
+            <option value="">Tutti i tipi</option>
+            <option value="type.id" v-for="type in store.types" :key="type.id">{{ type.name }}
+            </option>
+        </select>
+    </div>
 
-        <Router-link :to="{ name: 'single-project', params: { 'slug': project.slug }}">Visita project</Router-link>
-    </li>
-    </ul>
+    <div class="row">
+        <div class="col-12 col-lg-6" v-for="project in projects" :key="project.id">
+            <CardComponent :item="project"/>
+        </div>
+
+    </div>
 </template>
 
 <script>
     import {store} from '../store.js';
     import axios from 'axios';
+    import CardComponent from '../components/CardComponent.vue';
     export default {
     name: 'ProjectList',
+    components: {
+        CardComponent,
+    },
     data(){
         return{
         store,
